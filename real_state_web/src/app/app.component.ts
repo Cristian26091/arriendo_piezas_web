3dfe7099654b5947a9d3de0fd38e29f225dbe25b
Panda2609 <cbanda15@utalca.cl>
2022-11-10 18:12:29 -0300

    Creacion de los modulos (vistas) y creación de los componentes

--- real_state_web/src/app/app.component.ts
+++ real_state_web/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'real_state_web';
+  title = 'Sistema web de arriendo';
 }
