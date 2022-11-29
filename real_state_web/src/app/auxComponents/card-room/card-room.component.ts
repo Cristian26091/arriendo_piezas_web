bd3e34883d12cfa66033955bfdea4a09134af46d
Panda2609 <cbanda15@utalca.cl>
2022-11-29 03:44:13 -0300

    view changes

--- real_state_web/src/app/auxComponents/card-room/card-room.component.ts
+++ real_state_web/src/app/auxComponents/card-room/card-room.component.ts
@@ -7,8 +7,6 @@ import { Component } from '@angular/core';
 })
 export class CardRoomComponent{
 
-  longText = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown 
-  printer took a galley of type and scrambled it to make a type specimen book.`;
+  longText = `Lorem Ipsum is simply dummy text of the printing and typesetting industry.`
 
 }
