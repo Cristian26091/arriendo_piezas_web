27a8ac5c3af0f3f389fdfd099dfbe9c634b07654
Panda2609 <cbanda15@utalca.cl>
2023-03-22 12:36:17 -0300

    Prueba de objeto 3d (astronauta)

--- real_state_web/src/app/app.module.ts
+++ real_state_web/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion'
@@ -98,6 +98,9 @@ import { FilterResultComponent } from './auxComponents/filter-result/filter-resu
     MatSlideToggleModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ 
+    CUSTOM_ELEMENTS_SCHEMA 
+  ],
 })
 export class AppModule { }
